2a2f478f499c5710278c68b2969210d2e46aa32e
a.alekseev10 <a.alekseev10@tinkoff.ru>
2017-08-09 22:31:28 +0300

    refactor

---
+++ 02/01-2.js
@@ -0,0 +1,29 @@
+'use strict';
+
+Rx.Observable.create((observer) => {
+    observer.next(1);
+    observer.next(2);
+
+    setTimeout(() => {
+        observer.next(3);
+        observer.complete();
+    }, 1000);
+
+    setTimeout(() => {
+        observer.next(4);
+    }, 2000);
+}).subscribe((next) => {
+        console.log('--- next: ', next);
+    }, error => {
+        console.log('--- error', error);
+    },
+    () => {
+        console.log('done');
+    });
+
+document.addEventListener('click', onClick);
+// 'click' -> onClick();
+
+function handler() {}
+// ...
+const value = handler();
