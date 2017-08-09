a3d6116dab09e003a66c9ce7a36faf1923f96986
a.alekseev10 <a.alekseev10@tinkoff.ru>
2017-08-09 20:40:33 +0300

    demo 01

--- 01/01.js
+++ 01/01.js
@@ -3,31 +3,3 @@
 for (let letter of 'Observable') {
     console.log(letter);
 }
-
-class Observable {
-    constructor(source) {
-        this.source = source.split('');
-        this.result = this.source;
-    }
-
-    subscribe(next, done) {
-        for (let item of this.result) {
-            next(item);
-        }
-    }
-
-    filter(predicate) {
-        this.result = this.result.filter(predicate);
-        return this;
-    }
-
-    map(callback) {
-        this.result = this.result.map(callback);
-        return this;
-    }
-}
-
-new Observable('Observable')
-    .map(letter => letter.toUpperCase())
-    .filter(letter => letter === 'O')
-    .subscribe(letter => console.log(letter));
